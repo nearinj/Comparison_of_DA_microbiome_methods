14082682ffec10a5d67a2cdc8c198966fe956ef0
nearinj <jacob@vulcan.pharmacology.dal.ca>
2021-10-19 14:24:09 -0300

    formatted raw data tables

---
+++ Analysis_scripts/calc_bray_curt_permanova.R
@@ -0,0 +1,77 @@
+# Separate code to compute PERMANOVA on Aichison's distance between the two sample groups for each tested dataset in Fig 1.
+
+rm(list = ls(all.names = TRUE))
+
+library(parallelDist)
+library(vegan)
+
+
+# Read in ASV tables (unfiltered and filtered as well as rarefied or not for each).
+unfilt_study_tab  <- readRDS("/home/jacob/GitHub_Repos/Hackathon_testing/Data/unfilt_study_tab_21_04_07.RDS")
+filt_study_tab <- readRDS("/home/jacob/GitHub_Repos/Hackathon_testing/Data/filt_study_tab_21_04_07.RDS")
+
+# Read in sample groups for each dataset.
+meta_tables <- list()
+for (study in names(filt_study_tab$nonrare)) {
+
+  # The metadata table files have various file naming schemes so there are a few to try:
+  meta_path1 <- paste("/home/jacob/projects/HACKATHON_ANCOM_FIX_21_03_13/Hackathon/Studies/", study, "/", study, "_meta.tsv", sep = "")
+  meta_path2 <- paste("/home/jacob/projects/HACKATHON_ANCOM_FIX_21_03_13/Hackathon/Studies/", study, "/", study, "_metadata.tsv", sep = "")
+  meta_path3 <- paste("/home/jacob/projects/HACKATHON_ANCOM_FIX_21_03_13/Hackathon/Studies/", study, "/", study, "_metadata.csv", sep = "")
+  
+  if (file.exists(meta_path1)) {
+    meta_tables[[study]] <- read.table(file = meta_path1, header = TRUE, sep = "\t", row.names = 1, stringsAsFactors = FALSE)
+  } else if (file.exists(meta_path2)) {
+    meta_tables[[study]] <- read.table(file = meta_path2, header = TRUE, sep = "\t", row.names = 1, stringsAsFactors = FALSE)
+  } else if (file.exists(meta_path3)) {
+    meta_tables[[study]] <- read.table(file = meta_path3, header = TRUE, sep = "\t", row.names = 1, stringsAsFactors = FALSE)
+  } else {
+    print(study)
+    stop("Metadata file not found.")
+  }
+}
+
+
+
+PERMANOVA_output <- data.frame(matrix(NA, nrow = 38, ncol = 9))
+rownames(PERMANOVA_output) <- names(filt_study_tab$nonrare)
+colnames(PERMANOVA_output) <- c("dataset",
+                                "filt_nonrare_R2", "filt_nonrare_P", "filt_rare_R2", "filt_rare_P",
+                                "unfilt_nonrare_R2", "unfilt_nonrare_P", "unfilt_rare_R2", "unfilt_rare_P")
+PERMANOVA_output$dataset <- names(filt_study_tab$nonrare)
+
+num_threads <- 30
+
+for (study in names(filt_study_tab$nonrare)) {
+
+  message("Running ", study, " dataset")
+  
+  unfilt_nonrare_samples <- colnames(unfilt_study_tab$nonrare[[study]])[which(colnames(unfilt_study_tab$nonrare[[study]]) %in% rownames(meta_tables[[study]]))]
+  unfilt_nonrare_ASV_aitchison <- parallelDist(x = t(unfilt_study_tab$nonrare[[study]][, unfilt_nonrare_samples]), method = "bray", threads = num_threads)
+  unfilt_nonrare_ASV_aitchison_formula <- as.formula(unfilt_nonrare_ASV_aitchison ~ meta_tables[[study]][unfilt_nonrare_samples, 1])
+  unfilt_nonrare_ASV_aitchison_permanova <- data.frame(adonis(formula = unfilt_nonrare_ASV_aitchison_formula, permutations = 999)$aov.tab)
+  PERMANOVA_output[study, c("unfilt_nonrare_R2", "unfilt_nonrare_P")] <- as.numeric(unfilt_nonrare_ASV_aitchison_permanova[1, c("R2", "Pr..F.")])
+  
+  unfilt_rare_samples <- colnames(unfilt_study_tab$rare[[study]])[which(colnames(unfilt_study_tab$rare[[study]]) %in% rownames(meta_tables[[study]]))]
+  unfilt_rare_ASV_aitchison <- parallelDist(x = t(unfilt_study_tab$rare[[study]][, unfilt_rare_samples]), method = "bray", threads = num_threads)
+  unfilt_rare_ASV_aitchison_formula <- as.formula(unfilt_rare_ASV_aitchison ~ meta_tables[[study]][unfilt_rare_samples, 1])
+  unfilt_rare_ASV_aitchison_permanova <- data.frame(adonis(formula = unfilt_rare_ASV_aitchison_formula, permutations = 999)$aov.tab)
+  PERMANOVA_output[study, c("unfilt_rare_R2", "unfilt_rare_P")] <- as.numeric(unfilt_rare_ASV_aitchison_permanova[1, c("R2", "Pr..F.")])
+
+  filt_nonrare_samples <- colnames(filt_study_tab$nonrare[[study]])[which(colnames(filt_study_tab$nonrare[[study]]) %in% rownames(meta_tables[[study]]))]
+  filt_nonrare_ASV_aitchison <- parallelDist(x = t(filt_study_tab$nonrare[[study]][, filt_nonrare_samples]), method = "bray", threads = num_threads)
+  filt_nonrare_ASV_aitchison_formula <- as.formula(filt_nonrare_ASV_aitchison ~ meta_tables[[study]][filt_nonrare_samples, 1])
+  filt_nonrare_ASV_aitchison_permanova <- data.frame(adonis(formula = filt_nonrare_ASV_aitchison_formula, permutations = 999)$aov.tab)
+  PERMANOVA_output[study, c("filt_nonrare_R2", "filt_nonrare_P")] <- as.numeric(filt_nonrare_ASV_aitchison_permanova[1, c("R2", "Pr..F.")])
+  
+  filt_rare_samples <- colnames(filt_study_tab$rare[[study]])[which(colnames(filt_study_tab$rare[[study]]) %in% rownames(meta_tables[[study]]))]
+  filt_rare_ASV_aitchison <- parallelDist(x = t(filt_study_tab$rare[[study]][, filt_rare_samples]), method = "bray", threads = num_threads)
+  filt_rare_ASV_aitchison_formula <- as.formula(filt_rare_ASV_aitchison ~ meta_tables[[study]][filt_rare_samples, 1])
+  filt_rare_ASV_aitchison_permanova <- data.frame(adonis(formula = filt_rare_ASV_aitchison_formula, permutations = 999)$aov.tab)
+  PERMANOVA_output[study, c("filt_rare_R2", "filt_rare_P")] <- as.numeric(filt_rare_ASV_aitchison_permanova[1, c("R2", "Pr..F.")])
+  
+}
+
+write.table(x = PERMANOVA_output,
+            file = "/home/jacob/GitHub_Repos/Clean_Hackathon/Misc_datafiles/bray_permanova_results.tsv",
+            row.names = FALSE, col.names = TRUE, quote = FALSE, sep = "\t")
